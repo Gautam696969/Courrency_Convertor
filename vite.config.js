a69cfd0f6606d90d43b09e963d2d6390badd4144
Gautam696969 <palgautam69@gmail.com>
2025-06-03 13:49:58 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()],
-   base: '/Currency-Convertor/'
+   base: '/Currency_Convertor/'
 })
